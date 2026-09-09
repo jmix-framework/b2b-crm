fe91ac7048b0d2d64a8fc17a71145a151ed06e41
Mikhail Fedoseev <m.fedoseev@haulmont.com>
2026-09-09 19:04:34 +0400

    5119

--- src/main/java/com/company/crm/ai/view/component/menu/AddContextMenuFactory.java
+++ src/main/java/com/company/crm/ai/view/component/menu/AddContextMenuFactory.java
@@ -40,16 +40,16 @@ public class AddContextMenuFactory {
         addItem.setAriaLabel(messageBundle.getMessage("addContextAction"));
 
         SubMenu subMenu = addItem.getSubMenu();
-        subMenu.addItem(attachmentUpload, null);
+        subMenu.addItem(attachmentUpload, "");
 
-        MenuItem crmEntityItem = subMenu.addItem(
+        /*MenuItem crmEntityItem = subMenu.addItem(
                 createMenuItemContent(VaadinIcon.DATABASE, messageBundle.getMessage("addCrmEntityAction")),
                 null
         );
         SubMenu crmEntitySubMenu = crmEntityItem.getSubMenu();
         contextEntityRegistry.addMenuDefinitions()
                 .forEach(definition -> addEntityLookupMenuItem(crmEntitySubMenu, definition));
-
+*/
         return menuBar;
     }
 
